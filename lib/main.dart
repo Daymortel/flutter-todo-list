879c97e24d38d92f98d21b1a69160624fe9eed2f
Daymortel <daymortel@gmail.com>
2023-03-02 10:34:17 +0100

    base exo

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:exo3/widgets/my_home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({ super.key });
+
+  @override
+  Widget build(BuildContext context) {
+    String title = 'To-Do List';
+    return MaterialApp(
+      title: title,
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: MyHomePage(title: title)
+    );
+  }
+}
